f3e46e082db1558f20619346046c9873508d5d9e
caleb <caleb.schoepp@gmail.com>
2019-01-19 16:51:07 -0700

    I live and breathe boilerplate

---
+++ app/routes.py
@@ -0,0 +1,9 @@
+from flask import render_template, url_for, flash, redirect
+from app import app, db
+# from app.forms import AddGameForm
+from app.models import Game, Play
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', title='Home')
