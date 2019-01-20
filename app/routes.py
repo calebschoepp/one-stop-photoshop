f9f22a870ef6ddbd973e3b44460688fc7b2ba975
caleb <caleb.schoepp@gmail.com>
2019-01-20 05:58:45 -0700

    Some new stuff be going on

--- app/routes.py
+++ app/routes.py
@@ -1,27 +1,30 @@
 from flask import render_template, url_for, flash, redirect
 from app import app, db
 from app.models import Post
+from random import randrange as rr
 
 @app.route('/')
 @app.route('/index')
 def index():
-    posts = Post.query.all() # Builds a list
+    posts = Post.query.order_by(Post.folder).all() # Builds a list
     return render_template('index.html', posts=posts, title='One Stop Photoshop')
 
 
 
 ################### FIX
-@app.route('/next')
-def next():
-    return render_template('post.html', title='One Stop Photoshop')
+# @app.route('/next')
+# def next():
+#     return render_template('post.html', title='One Stop Photoshop')
 
-@app.route('/prev')
-def prev():
-    return render_template('post.html', title='One Stop Photoshop')
+# @app.route('/prev')
+# def prev():
+#     return render_template('post.html', title='One Stop Photoshop')
 
 @app.route('/random')
 def random():
-    return render_template('post.html', title='One Stop Photoshop')
+    rand = rr(0, Post.query.count())
+    post = Post.query.all()[rand]
+    return redirect('post/{}'.format(post.folder))
 
 @app.route('/post/<folder>')
 def post(folder):
