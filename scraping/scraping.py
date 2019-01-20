648f50c66b8ef34a46ba7efa543c6d46d2139d98
cmput274 <cmput274@ualberta.ca>
2019-01-19 20:11:40 -0700

    Added authentication() and implemented it in the newly created main()

--- scraping/scraping.py
+++ scraping/scraping.py
@@ -11,44 +11,43 @@
 
 import config
 import praw
-
-# START AUTHENTICATION
-
 import requests
 import requests.auth
 
-client_auth = requests.auth.HTTPBasicAuth(config.CLIENT_ID, config.CLIENT_SECRET)
-post_data = {"grant_type": "password", "username": REDDIT_USERNAME, "password": REDDIT_PASSWORD}
-headers = {"User-Agent": "linux:HackED-One-Stop-Photoshop:v0.1 (by u/HackED-Photoshop-Bot)"}
-response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
-response.json()
-{u'access_token': u'fhTdafZI-0ClEzzYORfBSCR7x3M',
- u'expires_in': 3600,
- u'scope': u'*',
- u'token_type': u'bearer'}
+def authentication():
 
-# Use the token:
-# Notice that for using the token, requests are made to https://oauth.reddit.com
-headers = {"Authorization": "bearer fhTdafZI-0ClEzzYORfBSCR7x3M", "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
-response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
-response.json()
-{u'comment_karma': 0,
- u'created': 1389649907.0,
- u'created_utc': 1389649907.0,
- u'has_mail': False,
- u'has_mod_mail': False,
- u'has_verified_email': None,
- u'id': u'1',
- u'is_gold': False,
- u'is_mod': True,
- u'link_karma': 1,
- u'name': u'HackED-Photoshop-Bot',
- u'over_18': True}
+    client_auth = requests.auth.HTTPBasicAuth(config.CLIENT_ID, config.CLIENT_SECRET)
+    post_data = {"grant_type": "password", "username": REDDIT_USERNAME, "password": REDDIT_PASSWORD}
+    headers = {"User-Agent": "linux:HackED-One-Stop-Photoshop:v0.1 (by u/HackED-Photoshop-Bot)"}
+    response = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=headers)
+    response.json()
+    {u'access_token': u'fhTdafZI-0ClEzzYORfBSCR7x3M',
+     u'expires_in': 3600,
+     u'scope': u'*',
+     u'token_type': u'bearer'}
 
-# END AUTHENTICATION
+    # Use the token:
+    # Notice that for using the token, requests are made to https://oauth.reddit.com
+    headers = {"Authorization": "bearer fhTdafZI-0ClEzzYORfBSCR7x3M", "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+    response = requests.get("https://oauth.reddit.com/api/v1/me", headers=headers)
+    response.json()
+    {u'comment_karma': 0,
+     u'created': 1389649907.0,
+     u'created_utc': 1389649907.0,
+     u'has_mail': False,
+     u'has_mod_mail': False,
+     u'has_verified_email': None,
+     u'id': u'1',
+     u'is_gold': False,
+     u'is_mod': True,
+     u'link_karma': 1,
+     u'name': u'HackED-Photoshop-Bot',
+     u'over_18': True}
 
 
 def main():
+
+    authentication()
 
     global myReddit
     global PSbattles
